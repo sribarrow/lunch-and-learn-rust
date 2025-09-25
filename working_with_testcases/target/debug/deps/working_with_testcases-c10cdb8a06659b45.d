a249894ab64f8ffe48bbf65d70eec7f51a42386b
Sri Barrow <sribarrow@gmail.com>
2025-09-25 10:27:26 +0100

    changes

--- working_with_testcases/target/debug/deps/working_with_testcases-c10cdb8a06659b45.d
+++
@@ -1,9 +0,0 @@
-/workspaces/lunch-and-learn-rust/working_with_testcases/target/debug/deps/libworking_with_testcases-c10cdb8a06659b45.rmeta: src/lib.rs Cargo.toml
-
-/workspaces/lunch-and-learn-rust/working_with_testcases/target/debug/deps/working_with_testcases-c10cdb8a06659b45.d: src/lib.rs Cargo.toml
-
-src/lib.rs:
-Cargo.toml:
-
-# env-dep:CLIPPY_ARGS=
-# env-dep:CLIPPY_CONF_DIR
